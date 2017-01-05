4d950a5219d8c7c8785a9e7700e32ad4dc0a381c
Ekaterina Goranova <epgoranova@gmail.com>
2017-01-05 22:11:41 +0200

    Refactor main file

--- main.cpp
+++ main.cpp
@@ -6,30 +6,6 @@
 
 const unsigned int BUF_SIZE = 1<<10;
 
-struct Reader {
-    int fd;
-
-    Reader(char* fileName);
-    ssize_t readInto(char*& buffer);
-};
-
-Reader::Reader(char* fileName) {
-    int fd = open(fileName, O_RDONLY);
-    if (fd == -1) {
-        perror("open");
-    }
-    this->fd = fd;
-}
-
-ssize_t Reader::readInto(char*& buffer) {
-    ssize_t r = read(this->fd, buffer, BUF_SIZE);
-
-    if (r <= 0) {
-        close(this->fd);
-    }
-    return r;
-}
-
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Expecting file name\n");
@@ -38,16 +14,17 @@ int main(int argc, char *argv[]) {
 
     SuffixAutomata* automata = new SuffixAutomata();
 
-    Reader* r = new Reader(argv[1]);
-    ssize_t n = 1;
-    char* b = new char[BUF_SIZE];
-    while (n > 0) {
-        n = r->readInto(b);
+    int fileDesc = open(argv[1], O_RDONLY);;
+    if (fileDesc == -1) perror("open");
+    ssize_t n = 0;
+    char* buffer = new char[BUF_SIZE];
+    do {
+        n = read(fileDesc, buffer, BUF_SIZE);
         for (int i = 0; i < n; i++) {
-            if (b[i] == '\n') break;
-            automata->add(b[i]);
+            if (buffer[i] == '\n') break;
+            automata->add(buffer[i]);
         }
-    }
+    } while (n > 0);
 
     int finalStatesCount = automata->setFinalStates();
     printf("states count: %d\ntransitions count: %d\nfinal count: %d\n",
